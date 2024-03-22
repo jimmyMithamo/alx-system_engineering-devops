3077c00209c874d6f29076eb916bc81ae3498913
jimmyMithamo <j1mithamo@gmail.com>
2024-03-23 00:18:26 +0300

    changes

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,8 @@
 # Install a package using Puppet
 
+package { 'python3-pip':
+  ensure => installed,
+}
 exec { 'install_flask':
   command => '/usr/bin/apt-get install flask==2.1.0',
   unless  => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
